ea6ae62b16a4ffb9fb06d45e811d8bcd548d7013
e34652 <e34652@naver.com>
2024-03-15 16:26:06 +0900

    24.03.15

--- javaScript/_todoList1/script.js
+++ javaScript/_todoList1/script.js
@@ -1,27 +1,35 @@
-const divEl = document.createElement('div'); //divEl = div 태그의 분신
-const pEl = document.createElement('p');//pEl = p 태그의 분신
-divEl.appendChild(pEl); // pEl을 divEl의 자식태그로 추가
-document.body.appendChild(divEl); // pEl이 자식태그로 있는 divEl을 body의 자식태그로 추가 <<body에 삽입
+//divEl = 생성된 div 요소를 담는 변수 El = element의 줄임말
+const divEl = document.createElement('div'); 
+
+//pEl = 생성된 p 요소를 담는 변수
+const pEl = document.createElement('p');
+
+// pEl를 divEl의 자식요소로 설정 
+divEl.appendChild(pEl);  
+
+// divEl를 body의 자식태그로 설정
+document.body.appendChild(divEl); 
+
 pEl.textContent = 'Hello World'; // pEl의 텍스트를 Hello World로 변경
 pEl.style.color = 'red'; //style태그의 color속성을 red로 변경
 
-const btnEl = document.createElement('button'); //button 태그의 분신
+const btnEl = document.createElement('button'); //생성된 button  태그를 담는 변수
 btnEl.textContent = 'Click Me'; //버튼의 텍스트를 Click Me로 변경
 divEl.appendChild(btnEl); // divEl의 자식 태그로 추가
 
-// document.body.removeChild(divEl);  << divEl의 내용 지우기
-// const bodyEl = document.querySelector('body'); << body의 전체 내용 지우기
-// bodyEl.textContent = null;
+// document.body.removeChild(divEl);  << body의 자식태그인 divEl 지우기
+// const bodyEl = document.querySelector('body'); << bodyEl을 body 태그 전체에 적용
+// bodyEl.textContent = null; << body의 전체 내용 지우기
 
 console.log('script.js');
-const title1 = document.getElementById('title'); // 하나의 아이디(ById)
+const title1 = document.getElementById('title'); // 하나의 아이디(ById), title과 연동
 console.log(title1);
 
-const title2 = document.querySelector('#title'); // 하나의 아이디(#)
+const title2 = document.querySelector('#title'); // 하나의 아이디(#), title과 연동
 console.log(title2);
 
-const con1 = document.getElementsByClassName('container'); // 모든 container Class
+const con1 = document.getElementsByClassName('container'); // 모든 container Class와 연동
 console.log(con1); 
 
-const con2 = document.querySelectorAll('.container'); // 모든 container Class(.)
+const con2 = document.querySelectorAll('.container'); // 모든 container Class(.)와 연동
 console.log(con2);
--- javaScript/_todoList2/index.js
+++ javaScript/_todoList2/index.js
@@ -1,7 +1,7 @@
-const onClick = () =>{ // 아래의 함수선언과 같음
+const onClick = function (){ // 아래의 함수선언과 같음
     const textEl = document.getElementById("add-text"); // textEl를 add-text와 연동
-    const text = textEl.value; // text의 값을 textEl.value로 설정
-    textEl.value = ""; // 이후 ""(공백)으로 초기화
+    const text = textEl.value; // text에 textEl.value값 저장
+    textEl.value = ""; // 값 저장 후 ""(공백)으로 초기화
 
 const li = document.createElement("li"); // li태그 생성
 
@@ -13,7 +13,7 @@ p.textContent = text; // p태그의 텍스트 값을 text로 설정
 const button = document.createElement("button"); // button 태그 생성
 button.textContent = "삭제"; // button의 text를 삭제로 설정
 
-
+//삭제버튼 클릭시 발생하는 이벤트 설정
 button.addEventListener("click",() => { 
 
     const deleteTarget = button.closest("li"); // 삭제 대상(target) 지정, closest() = 부모 요소와 일치하는 문자열을 찾는 메서드
@@ -25,13 +25,11 @@ div.appendChild(p); //div의 자식 태그로 p태그 추가
 
 div.appendChild(button) // button의 위치를 div의 자식 태그로 설정
 
-li.appendChild(div); //div의 자식 태그로 li태그 추가
+li.appendChild(div); //li의 자식 태그로 div태그 추가
 
 document.getElementById("memo-list").appendChild(li);
 }
-// const onClick2 = function(){ // 위의 람다식과 같음
-//     console.log('Button Clicked');
-// }
 
+//추가 버튼 클릭시 EventListener가 클릭을 감지하면 맨 위에 onClick 함수 실행
 document.getElementById('add-button').addEventListener('click', onClick);
-//버튼 클릭시 EventListener가 클릭을 감지하면 맨 위에 onClick 함수 실행
+
